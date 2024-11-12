1dbbb1f2e423fa431dcd14f14789cc45e4c7f44e
Ashrith <ashrithnamila@gmail.com>
2024-11-13 00:10:51 +0530

    Implemented game structure with Dice, Player, and Arena classes for magical arena

---
+++ src/classes/Arena.js
@@ -0,0 +1,50 @@
+const Player = require('./player').default;
+
+class Arena {
+    constructor(playerA, playerB) {
+        this.playerA = playerA;
+        this.playerB = playerB;
+        this.gameWinner = null;
+    }
+
+    fight() {
+        let attacker, defender;
+        if (this.playerA.health <= this.playerB.health) {
+            attacker = this.playerA;
+            defender = this.playerB;
+        } else {
+            attacker = this.playerB;
+            defender = this.playerA;
+        }
+
+        while (attacker.isAlive() && defender.isAlive()) {
+
+            console.log(`\n${attacker.name} attacks ${defender.name}`);
+
+            const attack = attacker.attack();
+            const defence = defender.defend();
+            let netDamage = parseInt( Math.floor(Math.max(attack.attackingDamage - defence.defencePower, 0)));
+
+            if (attacker.attackPower) {
+                console.log(`${attacker.name} rolled a dice value of ${attack.diceValue}. Total Attack Damage: ${attack.attackingDamage}.`);
+            }
+            if (defender.strength) {
+                console.log(`${defender.name} rolled a dice value of ${defence.diceValue}. Total Defense Power: ${defence.defencePower}.`);
+            }
+
+            console.log(`${defender.name} takes ${netDamage} damage.`);
+            defender.receiveDamage(netDamage);
+            console.log(`${defender.name}'s health: ${defender.health}`);
+
+            [attacker, defender] = [defender, attacker];
+        }
+
+        this.gameWinner = attacker.isAlive() ? attacker : defender;
+
+        console.log(`\n${this.gameWinner.name} wins the fight with ${this.gameWinner.health} health remaining.`);
+
+
+    }
+}
+
+module.exports = Arena;
